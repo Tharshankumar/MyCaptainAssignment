1f10bf10817f66791f175b9b4ee395a7f30049f1
Tharshankumar <89776034+Tharshankumar@users.noreply.github.com>
2022-07-29 19:12:35 +0530

    Add files via upload

---
+++ Fibonacci_numbers.py
@@ -0,0 +1,22 @@
+#The Fibonacci numbers are the numbers in the following integer sequence. 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+
+nterms = int(input("How many terms? "))
+
+n1, n2 = 0, 1
+count = 0
+
+if nterms <= 0:
+   print("Please enter a positive integer")
+
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
